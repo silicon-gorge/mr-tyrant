13e8ace313ba9e574e68fa83341b1940bf319c35
Neil Prosser <neil.prosser@nokia.com>
2014-03-03 16:32:15 +0000

    Remove per-resource metrics

--- src/tyranitar/web.clj
+++ src/tyranitar/web.clj
@@ -15,8 +15,6 @@
             [clojure.tools.logging :refer [info warn error]]
             [environ.core :refer [env]]
             [nokia.ring-utils.error :refer [wrap-error-handling error-response]]
-            [nokia.ring-utils.metrics :refer [wrap-per-resource-metrics replace-outside-app
-                                              replace-guid replace-mongoid replace-number]]
             [nokia.ring-utils.ignore-trailing-slash :refer [wrap-ignore-trailing-slash]]
             [metrics.ring.expose :refer [expose-metrics-as-json]]
             [metrics.ring.instrument :refer [instrument]]
@@ -145,5 +143,4 @@
       (wrap-keyword-params)
       (wrap-params)
       (wrap-json-response)
-      (wrap-per-resource-metrics [replace-guid replace-mongoid replace-number (replace-outside-app "/1.x")])
       (expose-metrics-as-json)))
